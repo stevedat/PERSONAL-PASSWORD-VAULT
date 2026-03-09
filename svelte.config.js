6ff29366f297dae3457193ced0c84d7c501ce8b7
ttd1232004 <ttd1232004@gmail.com>
2026-03-09 01:17:24 +0000

    Fix: Configure Vercel adapter and Node.js runtime

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,13 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: vitePreprocess(),
   kit: {
-    adapter: adapter(),
+    adapter: adapter({
+      runtime: 'nodejs20.x'
+    }),
     alias: {
       $lib: 'src/lib',
       '$lib/*': 'src/lib/*'
